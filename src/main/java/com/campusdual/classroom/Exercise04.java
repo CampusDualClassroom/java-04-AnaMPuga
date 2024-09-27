ff4c9f761400a2603d60a7a53f5d0668c24c2a3f
Ana Martínez Puga <anammpuga@gmail.com>
2024-09-27 11:47:04 +0200

    Calcular sumas y factoriales con bucles

--- src/main/java/com/campusdual/classroom/Exercise04.java
+++ src/main/java/com/campusdual/classroom/Exercise04.java
@@ -5,26 +5,59 @@ public class Exercise04 {
 
         System.out.println("Ejemplos de cálculos SIN estructuras de control:");
         System.out.println("Suma de los primeros 5 números: " + (1 + 2 + 3 + 4 + 5));
+
+
         System.out.println("Suma de los primeros 20 números pares: " + (2 + 4 + 6 + 8 + 10 + 12 + 14 + 16 + 18 + 20 + 22 + 24 + 26 + 28 + 30 + 32 + 34 + 36 + 38 + 40));
+
+
         System.out.println("Obtén el factorial del número 7 → (7!): " + (7 * 6 * 5 * 4 * 3 * 2 * 1));
+
+
         System.out.println("######################################################################");
         System.out.println("Ejemplos de cálculos CON estructuras de control:");
+        sumNum(5);
+        sumEvenNum(20);
+        factorial(7);
+        recursiveFactorial(7);
+
 
     }
 
     public static int sumNum(int num) {
-        return 0;
+        int resultado=0;
+        for (int i= 0; i<=num; i++){
+            resultado = resultado+i;
+
+        }
+        System.out.println("Suma de los primeros números: " + resultado);
+
+        return resultado;
     }
 
     public static int sumEvenNum(int num) {
-        return 0;
+        int resultado= 0;
+        for (int i= 0; i<=num; i++){
+            resultado = resultado +i*2;
+        }
+        System.out.println("El resultado de sumar números pares es: " + resultado);
+        return resultado;
     }
 
     public static int factorial(int num) {
-        return 0;
+        int resultado = 1;
+        for (int i= 1; i<num+1; i++){
+            resultado = resultado*i;
+        }
+        System.out.println("El factorial es: " + resultado);
+        return resultado;
     }
 
     public static int recursiveFactorial(int num) {
-        return 0;
+        if (num <=1) {
+            return num;
+        } else {
+            return num * factorial(num - 1);
+        }
+
     }
 }
